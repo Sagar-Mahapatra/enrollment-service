5dd3e36925cede9fe17721bccdbb1c12d11ce882
Sagar Mahapatra <sagarkmohapatra2012@gmail.com>
2022-03-22 14:07:29 +0530

    initial commit

---
+++ src/main/java/com/keybank/controller/EnrollmentRestController.java
@@ -0,0 +1,22 @@
+/** @Copyright 2022 keybank pvt ltd, all rights are reserved, you shouldn't disclose the information outside otherwise terms and condition will apply.
+ * 
+ */
+package com.keybank.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Sagar, 22-Mar-2022
+ *
+ * Description: 
+ */
+@RestController
+@RequestMapping("/v1")
+public class EnrollmentRestController {
+	
+	
+	
+	
+
+}
---
+++ src/main/java/com/keybank/service/EnrollmentServiceImpl.java
@@ -0,0 +1,16 @@
+/** @Copyright 2022 keybank pvt ltd, all rights are reserved, you shouldn't disclose the information outside otherwise terms and condition will apply.
+ * 
+ */
+package com.keybank.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Sagar, 22-Mar-2022
+ *
+ * Description: 
+ */
+@Service
+public class EnrollmentServiceImpl {
+
+}
